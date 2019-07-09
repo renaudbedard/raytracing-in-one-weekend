6fcc3ff4c76d649a0a353631b64bb37579762b30
Renaud Bédard <renaud.bedard@gmail.com>
2019-07-08 20:52:50 -0400

    WIP camera control

---
+++ RaytracingInOneWeekend/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputSystem.Plugins.PlayerInput;
+
+namespace RaytracerInOneWeekend
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class CameraControl : MonoBehaviour
+    {
+        void Update()
+        {
+            
+        }
+    }
+}
