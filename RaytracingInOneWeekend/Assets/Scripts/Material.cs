955d4688ce216525a1d8ff5c1c261f401fc04812
Renaud Bédard <rbedard@square-enix-montreal.com>
2019-10-01 13:21:17 -0400

    WIP GGX

--- RaytracingInOneWeekend/Assets/Scripts/Material.cs
+++ RaytracingInOneWeekend/Assets/Scripts/Material.cs
@@ -26,7 +26,7 @@ namespace RaytracerInOneWeekend
 		public readonly float Parameter;
 
 		public Material(MaterialType type, float2 textureScale, Texture albedo = default,
-			Texture emission = default, float fuzz = 0, float refractiveIndex = 1, float density = 1) : this()
+			Texture emission = default, float roughness = 0, float refractiveIndex = 1, float density = 1) : this()
 		{
 			Type = type;
 			TextureScale = textureScale;
@@ -34,7 +34,7 @@ namespace RaytracerInOneWeekend
 			switch (type)
 			{
 				case MaterialType.Lambertian: Texture = albedo; break;
-				case MaterialType.Metal: Parameter = saturate(fuzz); Texture = albedo; break;
+				case MaterialType.Metal: Parameter = saturate(roughness); Texture = albedo; break;
 				case MaterialType.Dielectric: Parameter = refractiveIndex; break;
 				case MaterialType.DiffuseLight: Texture = emission; break;
 				case MaterialType.Isotropic: Parameter = density; Texture = albedo; break;
@@ -42,7 +42,7 @@ namespace RaytracerInOneWeekend
 		}
 
 		[Pure]
-		public bool Scatter(Ray r, HitRecord rec, ref Random rng, PerlinData perlinData,
+		public bool Scatter(Ray ray, HitRecord rec, ref Random rng, PerlinData perlinData,
 			out float3 albedo, out Ray scattered)
 		{
 			switch (Type)
@@ -51,48 +51,69 @@ namespace RaytracerInOneWeekend
 				{
 					albedo = Texture.Value(rec.Point, rec.Normal, TextureScale, perlinData);
 					float3 randomDirection = rng.OnUniformHemisphere(rec.Normal);
-					scattered = new Ray(rec.Point, randomDirection, r.Time);
+					scattered = new Ray(rec.Point, randomDirection, ray.Time);
 					return true;
 				}
 
 				case MaterialType.Metal:
 				{
-					float fuzz = Parameter;
-					float3 reflected = reflect(r.Direction, rec.Normal);
 					albedo = Texture.Value(rec.Point, rec.Normal, TextureScale, perlinData);
-					scattered = new Ray(rec.Point, normalize(reflected + fuzz * rng.NextFloat3Direction()), r.Time);
+
+					// https://computergraphics.stackexchange.com/questions/7656/importance-sampling-microfacet-ggx
+					// r0 := rnd.Float64()
+					// r1 := rnd.Float64()
+					// a := m.Roughness * m.Roughness
+					// a2 := a * a
+					// theta := math.Acos(math.Sqrt((1 - r0) / ((a2-1)*r0 + 1)))
+					// phi := 2 * math.Pi * r1
+					// x := math.Sin(theta) * math.Cos(phi)
+					// y := math.Cos(theta)
+					// z := math.Sin(theta) * math.Sin(phi)
+					// wm := geom.Vector3{x, y, z}.Unit()
+					// wi := wo.Reflect2(wm)
+					// return wi
+
+					float2 r = rng.NextFloat2();
+					float a = Parameter * Parameter;
+					float a2 = a * a;
+					float theta = acos(sqrt((1 - r[0]) / ((a2 - 1) * r[0] + 1)));
+					float phi = 2 * PI * r[1];
+					sincos(float2(theta, phi), out float2 sinThetaPhi, out float2 cosThetaPhi);
+					float3 wm = normalize(float3(sinThetaPhi.x * cosThetaPhi.y, cosThetaPhi.x, sinThetaPhi.x * sinThetaPhi.y));
+					float3 wi = reflect(ray.Direction, wm);
+					scattered = new Ray(rec.Point, wi);
 					return true;
 				}
 
 				case MaterialType.Dielectric:
 				{
 					float refractiveIndex = Parameter;
-					float3 reflected = reflect(r.Direction, rec.Normal);
+					float3 reflected = reflect(ray.Direction, rec.Normal);
 					albedo = 1;
 					float niOverNt;
 					float3 outwardNormal;
 					float cosine;
 
-					if (dot(r.Direction, rec.Normal) > 0)
+					if (dot(ray.Direction, rec.Normal) > 0)
 					{
 						outwardNormal = -rec.Normal;
 						niOverNt = refractiveIndex;
-						cosine = refractiveIndex * dot(r.Direction, rec.Normal);
+						cosine = refractiveIndex * dot(ray.Direction, rec.Normal);
 					}
 					else
 					{
 						outwardNormal = rec.Normal;
 						niOverNt = 1 / refractiveIndex;
-						cosine = -dot(r.Direction, rec.Normal);
+						cosine = -dot(ray.Direction, rec.Normal);
 					}
 
-					if (Refract(r.Direction, outwardNormal, niOverNt, out float3 refracted))
+					if (Refract(ray.Direction, outwardNormal, niOverNt, out float3 refracted))
 					{
 						float reflectProb = Schlick(cosine, refractiveIndex);
-						scattered = new Ray(rec.Point, rng.NextFloat() < reflectProb ? reflected : refracted, r.Time);
+						scattered = new Ray(rec.Point, rng.NextFloat() < reflectProb ? reflected : refracted, ray.Time);
 					}
 					else
-						scattered = new Ray(rec.Point, reflected, r.Time);
+						scattered = new Ray(rec.Point, reflected, ray.Time);
 
 					return true;
 				}
@@ -124,8 +145,23 @@ namespace RaytracerInOneWeekend
 			Validate(scattered, rec);
 			switch (Type)
 			{
-				case MaterialType.Lambertian: return max(dot(rec.Normal, scattered.Direction) / PI, 0);
-				case MaterialType.Isotropic: return 1.0f / (4.0f * PI);
+				case MaterialType.Lambertian:
+					return max(dot(rec.Normal, scattered.Direction) / PI, 0);
+
+				case MaterialType.Isotropic:
+					return 1.0f / (4.0f * PI);
+
+				// https://computergraphics.stackexchange.com/questions/7656/importance-sampling-microfacet-ggx
+				// case MaterialType.Metal:
+				// 	wg := geom.Up
+				// 	wm := wo.Half(wi)
+				// 	a := m.Roughness * m.Roughness
+				// 	a2 := a * a
+				// 	cosTheta := wg.Dot(wm)
+				// 	exp := (a2-1)*cosTheta*cosTheta + 1
+				// 	D := a2 / (math.Pi * exp * exp)
+				// 	return (D * wm.Dot(wg)) / (4 * wo.Dot(wm))
+
 				default: throw new NotImplementedException();
 			}
 		}
